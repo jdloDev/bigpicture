1818f5db90daa456104b2ecd646058b705c22dbd
jdloDev <jdlo.dev@gmail.com>
2025-07-24 11:15:24 +0200

    Update Startup.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
-
+// trihher a PR verification  build
 namespace Tailwind.Traders.Web
 {
     public class Startup
